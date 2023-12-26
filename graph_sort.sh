bce4a1f971a5d02b886f03192fa4c031fab4251c
Adam <aterrak@adam-ubuntu.home>
2023-12-26 23:24:01 +0100

    -d1 almost done, just gnuplot left

--- graph_sort.sh
+++ graph_sort.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+if [ ! -d "temp" ]; then #Check if temp/ exist, else create it and give all acces
+    mkdir temp
+    chmod u+r+w+x temp
+
+elif [ -s "temp" ]; then #Check if temp is empty, else rm its content
+    rm -f -r -d temp/*
+fi
+
+if [ ! -d "pictures" ]; then #Check if pictures/ exist, else create it and give all acces
+    mkdir pictures
+    chmod u+r+w+x pictures
+fi
+
+for i in ${tmp#*" "}; do #if arg? == -h || --help, arrays help and exit 0
+    if [ $i = "-h" ] || [ $i = "--help" ]; then
+        echo help msg
+        exit 0
+    fi
+done
+
 if (($# <= 1)); then #Check if at least 2 args
     echo "no argument dected"
     exit 1
@@ -15,34 +35,38 @@ if [ $(echo ${1##*.}) != "csv" ]; then #Check if arg1 is a .csv file
     exit 3
 fi
 
-if [ ! -d "temp" ]; then #Check if temp/ exist, else create it and give all acces
-    mkdir temp
-    chmod u+r+w+x temp
-
-elif [ -s "temp" ]; then #Check if temp is empty, else rm its content
-    rm -f -r -d temp/*
-fi
-
-if [ ! -d "pictures" ]; then #Check if pictures/ exist, else create it and give all acces
-    mkdir pictures
-    chmod u+r+w+x pictures
-fi
-
 tmp=$@
-
-for i in ${tmp#*" "}; do #if arg? == -h || --help, arrays help and exit 0
-    if [ $i = "-h" ] || [ $i = "--help" ]; then
-        echo help msg
-        exit 0
-    fi
-done
 
 for i in ${tmp#*" "}; do
 
     case $i in
 
     -d1) #10 drivers with most rides
-        echo d1
+        SECONDS=0
+        
+        #awk part
+
+        cut -d';' -f1,6 < $1 | awk -F';' '!arr[$1]++ {arr2[$2]++} END {for (i in arr2) print i ";" arr2[i]}' | sort -t';' -k2,2nr | head -n 10 > temp/d1.temp
+        
+        # cut the data.csv file to just keep column 1 and 6
+        # takes the duplicates of rides ID away using a arr to see if the ID were encountred already
+        # next, counts the occurences of names using arr2 and separate them by ';' in the output
+        # after all this, sort the output of names numerically by occurences and from the highest to lowest
+        # using ; as delimiter. To finish, take the 10 first lines of the output
+        # finally it sends all in the d1.temp file in temp/
+
+        gnuplot <<-EOFMarker
+    set terminal png size 800,600
+    set output 'pictures/d1.png'
+    set style data histeps
+    set style fill solid
+    set boxwidth 0.8
+    set datafile separator ";"
+    set title '10 drivers with most rides'
+    plot 'temp/d1.temp' using 2:ytic(1) notitle 
+EOFMarker
+        
+        echo -e "\nelapsed time for d1: $SECONDS seconds" #la var SECONDS contient le temps écoulé (SECONDS="0" pour réinitialiser)
         ;;
 
     -d2) #10 drivers with longest distances
@@ -61,5 +85,3 @@ for i in ${tmp#*" "}; do
 
     esac
 done
-
-echo elapsed time: $SECONDS seconds #la var SECONDS contient le temps écoulé (SECONDS="0" pour réinitialiser)
